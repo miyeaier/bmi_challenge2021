2aa12f6c5af28a15153ac2ffa9c95c5cf8cf68ab
miyeaier <miyesier@gmail.com>
2021-11-16 19:22:55 +0100

    change input id to name everthing working

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,16 @@
 const BMI = new BMICalculator();
-const weightInput = document.querySelector("input[id=weight]");
-const heightInput = document.querySelector("input[id=height]");
+const weightInput = document.querySelector("input[name=weight]");
+const heightInput = document.querySelector("input[name=height]");
 const button = document.getElementById("submit-button");
 const messageElement = document.getElementById('return-MESSAGE');
 
 button.addEventListener('click', () => {
+  
   if (weightInput.value === ''){
-    messageElement.innerText =
-    "HEY,YOU NEED TO GIVE US YOUR Weight";
+    debugger
+    messageElement.innerText ="HEY,YOU NEED TO GIVE US YOUR Weight";
   } else if ((weightInput.value === '') || (heightInput.value === '')){
-    messageElement.innerText = "HEY,YOU NEED TO GIVE US YOUR Weight AND HEIGHT"
+    messageElement.innerText ="HEY,YOU NEED TO GIVE US YOUR Weight AND HEIGHT";
   }else{
     const BMIResult = BMI.calculateMetric({ height: heightInput.value, weight: weightInput.value });
         messageElement.innerText = `BMI: ${BMIResult.value} - ${BMIResult.classification}`;
