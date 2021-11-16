08b8c3810adfea10a949503d7f2fe9e10f09eb6f
miyeaier <miyesier@gmail.com>
2021-11-16 18:53:12 +0100

    everthing working test go green too

--- src/js/bmiCalculator.js
+++ src/js/bmiCalculator.js
@@ -1,7 +1,8 @@
-class BMICalculator{
+
+class BMICalculator {
   calculateMetric(options) {
     const weight = options.weight;
-    const height = options.height;
+    const height = options.height/100;
     const bmiValue = (weight / (height * height)).toFixed(2);
     const bmiResults = {
       value: parseFloat(bmiValue),
@@ -13,20 +14,25 @@ class BMICalculator{
   getBMIClassification(value){
     if (value < 18.5){
       return 'Underweight'
-    }else if (value <24.9) {
+    }
+    else if (value <24.9){
       return'Normal weight'
-    }else if (value < 29.9){
+    }
+    else if (value < 29.9){
       return'Overweight'
-    }else if (value <34.9){
+    }
+    else if (value <34.9){
       return'Obesity Class 1'
-    }else if (value <39.9) {
+    }
+    else if (value <39.9) {
       return'Obesity Class 2'
-    }else {
+    }
+    else {
       return 'Extreme Obesity Class 3'
     }
   }
 }
 
-if (typeof module !== 'undefined' && module.exports){
- module.exports = BMICalculator
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = BMICalculator
 }
